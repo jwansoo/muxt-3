b703290235a4045b53df4d6ab75c22def271564a
wansoo <jwansoo5197@gmail.com>
2024-08-28 16:31:40 +0900

    mast-4

--- pages/course.vue
+++ pages/course.vue
@@ -1,18 +1,22 @@
 <script setup>
-const { chapters } = useCourse();
+const { chapters, title } = useCourse();
+const resetError = async (error) => {
+  await navigateTo(
+    "/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3"
+  );
+  error.value = null;
+};
 </script>
 
 <template>
-  <div
-    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
-  >
-    <div class="prose mb-12">
-      <h1>
+  <div>
+    <div class="mb-4 flex justify-between items-center w-full">
+      <h1 class="text-3xl">
         <span class="font-medium">
-          Course:
-          <span class="font-bold">Mastering Nuxt 3</span>
+          <span class="font-bold">{{ title }}</span>
         </span>
       </h1>
+      <UserCard />
     </div>
 
     <div class="flex flex-row justify-center flex-grow">
@@ -43,8 +47,24 @@ const { chapters } = useCourse();
         </div>
       </div>
 
-      <div class="prose p-12 bg-white rounded-md w-[65ch]">
-        <NuxtPage />
+      <div class="prose p-12 bg-white rounded-md w-[75ch]">
+        <NuxtErrorBoundary>
+          <NuxtPage />
+          <template #error="{ error }">
+            <p>
+              Oh no, something went wrong with the lesson!
+              <code>{{ error }}</code>
+            </p>
+            <p>
+              <button
+                class="hover:cursor-pointer bg-gray-500 text-white font-bold py-1 px-3 rounded"
+                @click="resetError(error)"
+              >
+                Reset
+              </button>
+            </p>
+          </template>
+        </NuxtErrorBoundary>
       </div>
     </div>
   </div>
